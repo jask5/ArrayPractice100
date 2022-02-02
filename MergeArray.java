dddbb1788480240e6255f5c43a4f314132585e01
jask5 <95156965+jask5@users.noreply.github.com>
2022-02-01 19:41:21 -0800

    Add files via upload

---
+++ MergeArray.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+
+public class MergeArray {
+    //merging two arrays after comparing them
+    private int[] mergeArrays(int[] arr1, int[] arr2){
+        //function to take the two arrays 
+        //index of arr1, arr2 & merged array
+
+        int i = 0, j = 0, k = 0;
+        
+        //merged array length
+        int mergedArrayLength = arr1.length + arr2.length;
+
+        //merged array 
+        int[] mergedArray = new int[mergedArrayLength];
+        
+
+        //checking elements in both the arrays and adding smaller ones first 
+        while(i < arr1.length && j < arr2.length) {
+            if(arr1[i] < arr2[j]) {
+                mergedArray[k] = arr1[i];
+                i++;
+            
+            } else {
+                mergedArray[k] = arr2[j];
+                j++;
+            }
+            k++;
+
+        }
+
+
+        while(i < arr1.length) {
+            mergedArray[k] = arr1[i];
+            i++;
+            k++;
+        }
+        while(j < arr2.length) {
+            mergedArray[k] = arr2[j];
+            j++;
+            k++;
+        }
+
+        return mergedArray;
+    }
+    
+
+public static void main(String[] args) {
+    MergeArray arrays = new MergeArray();
+    int[] arr3 = arrays.mergeArrays(new int[] {0,3,4,31}, new int[] {4, 6, 30});
+    System.out.println(Arrays.toString(arr3));
+}
+}
