335ae2d46ed79b026043c47798503fa901cc61ba
jask5 <95156965+jask5@users.noreply.github.com>
2022-02-10 10:21:02 -0800

    Add files via upload

---
+++ SwapArray.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+
+//swaping the arrays based on index 
+public class SwapArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        swap(arr, 0, 1); //0 and 1 index swapped
+        System.out.println(Arrays.toString(arr));
+
+
+    }
+
+    static void swap(int[] arr, int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+    
+}
