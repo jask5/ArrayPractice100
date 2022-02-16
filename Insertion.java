cce2be204f7d1bdddf2d807604b44c37052007f6
Jaskirat Kaur <95156965+jask5@users.noreply.github.com>
2022-02-16 11:43:25 -0800

    Add files via upload

---
+++ Insertion.java
@@ -0,0 +1,26 @@
+package JK.company;
+import java.util.*;
+
+public class Insertion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+
+        }
+        for(int i = 0; i < n; i++) {
+            int current = arr[i];
+            int j = i-1;
+            while(j >= 0 && arr[j] > current) {
+                arr[j+1] = arr[j];
+                j = j-1;
+                arr[j+1] = current;
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
